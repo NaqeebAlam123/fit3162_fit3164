03bef1093c3c039aab63f78fac90d366f3d59958
mala0036 <mala0036@student.monash.edu>
2022-10-03 13:52:53 +0000

    Added documentation for lm_pca.py

--- custom/code/audio2lm/lm_pca.py
+++ custom/code/audio2lm/lm_pca.py
@@ -102,66 +102,82 @@ def parameter_reader(flist):
 
 #mfcc file split
 # p = Path('train/MFCC/M030/')
-mfcc_dataset = Path(EMOTION_NET_DATASET_DIR)
+def lm_pca():
+    """
+    This function is responsible for combining feature vectors residing in mfcc npy files generated from raw audio to reduce into even less number of npy files.
+    25 sized overlappong window is rolled over the npy files to perform the integration.The path to these npy values is stored and segregated into training and validation.
+    mean and standard deviation is also calculated using the data stored in npy values.
+    The quantifiers as well as raw integrated mfcc files in combination with aligned landmarks will then be used to enode landmarks and train audio to landmark module
+    """
+    # load mfcc files generated from raw audio
+    mfcc_dataset = Path(EMOTION_NET_DATASET_DIR)
 
-for path in mfcc_dataset.iterdir():
-    _emotion, idx = path.name.split('_')[0], path.name.split('_')[1]
-    '''
-    e.g.: path = 'fear_025'
-    _emotion = 'fear'
-    idx = 025
-    '''
-    if _emotion == 'neutral':
-        outpath = f'{LM_ENCODER_DATASET_MFCC_DIR}M030_neutral_1_{idx}/'
-    else:
-        outpath = f'{LM_ENCODER_DATASET_MFCC_DIR}M030_{_emotion}_3_{idx}/'
-
-    os.makedirs(outpath, exist_ok=True)
-    mfcc_all = []
-    n_mfcc_files = len(list(path.iterdir()))
-    for i in range(n_mfcc_files):
-        mfcc = np.load(path.joinpath(str(i)+'.npy'))
-        mfcc_all.append(mfcc)
-    mfcc_all = np.array(mfcc_all)
-
-    for i in range((n_mfcc_files-4)//25):
-        mfcc_save = np.array(mfcc_all[i*25:i*25+25])
-        np.save(f'{outpath}{str(i)}.npy', mfcc_save)
-
-
-train_list, val_list = [], []
-# mfcc_dataset = Path('train/landmark/dataset_M030/mfcc/')
-mfcc_dataset = Path(LM_ENCODER_DATASET_MFCC_DIR)
-for path in mfcc_dataset.iterdir():
-    for mpath in path.iterdir():
-        if not Path(f'{LM_ENCODER_DATASET_LANDMARK_DIR}{path.name}/{mpath.name}').is_file():
-            continue
-        if int(path.name.split('_')[3]) < 10:
-            val_list.append(f'{path.name}/{mpath.name}')
+    # traverse different emotional audio folders
+    for path in mfcc_dataset.iterdir():
+        _emotion, idx = path.name.split('_')[0], path.name.split('_')[1]
+        '''
+        e.g.: path = 'fear_025'
+        _emotion = 'fear'
+        idx = 025
+        '''
+        if _emotion == 'neutral':
+            outpath = LM_ENCODER_DATASET_MFCC_DIR+"M030_neutral_1_"+idx+"/"
         else:
-            train_list.append(f'{path.name}/{mpath.name}')
+            outpath = LM_ENCODER_DATASET_MFCC_DIR+"M030_"+_emotion+"_3_"+idx+"/"
 
-with open('data/landmark/landmark_68/M030/basics/train_M030.pkl', 'wb') as f:
-    pickle.dump(train_list, f)
-with open('data/landmark/landmark_68/M030/basics/val_M030.pkl', 'wb') as f:
-    pickle.dump(val_list, f)
+        os.makedirs(outpath, exist_ok=True)
+        mfcc_all = []
+        # gather data in mfcc files from particular emotional audio folders and store in list
+        n_mfcc_files = len(list(path.iterdir()))
+        for i in range(n_mfcc_files):
+            mfcc = np.load(path.joinpath(str(i)+'.npy'))
+            mfcc_all.append(mfcc)
+        mfcc_all = np.array(mfcc_all)
 
-lm_all = []
-for i in range(len(train_list)):
-    path = f'{LM_ENCODER_DATASET_LANDMARK_DIR}{train_list[i]}'
-    a = np.load(path)
-    lm_all.append(a)
+        # joining mfcc data
+        for i in range((n_mfcc_files-4)//25):
+            mfcc_save = np.array(mfcc_all[i*25:i*25+25])
+            np.save(outpath+str(i)+".npy", mfcc_save)
 
-lm = np.array(lm_all)
+    # segregating it into training and validation lists
+    train_list, val_list = [], []
+    mfcc_dataset = Path(LM_ENCODER_DATASET_MFCC_DIR)
+    for path in mfcc_dataset.iterdir():
+        for mpath in path.iterdir():
+            if not Path(LM_ENCODER_DATASET_LANDMARK_DIR+path.name+"/"+mpath.name).is_file():
+                continue
+            # split between data using the index such that less than 10 are added to validation
+            if int(path.name.split('_')[3]) < 10:
+                val_list.append(path.name+"/"+mpath.name)
+            else:
+                train_list.append(path.name+"/"+mpath.name)
+    # store the training and validation lists in pickle files
+    with open('data/landmark/landmark_68/M030/basics/train_M030.pkl', 'wb') as f:
+        pickle.dump(train_list, f)
+    with open('data/landmark/landmark_68/M030/basics/val_M030.pkl', 'wb') as f:
+        pickle.dump(val_list, f)
 
-lm = lm.reshape(len(train_list)*25,136)
+    # loading training list data, calculating mean and standard deviation and storing in file
+    lm_all = []
+    for i in range(len(train_list)):
+        path = LM_ENCODER_DATASET_LANDMARK_DIR+train_list[i]
+        a = np.load(path)
+        lm_all.append(a)
 
-mean = np.mean(lm, axis=0)
-lm1 = lm - mean
+    lm = np.array(lm_all)
 
-lm1 = np.array(lm1)
-U,s,V = np.linalg.svd(lm1.T)
-# print(f'U shape {U.shape} mean shape {mean.shape}')
+    lm = lm.reshape(len(train_list)*25,136)
 
-np.save(f'{LANDMARK_BASICS}U_68.npy', U)
-np.save(f'{LANDMARK_BASICS}mean_68.npy', mean)
+    mean = np.mean(lm, axis=0)
+    lm1 = lm - mean
+
+    lm1 = np.array(lm1)
+    U,s,V = np.linalg.svd(lm1.T)
+
+    np.save(LANDMARK_BASICS+"U_68.npy", U)
+    np.save(LANDMARK_BASICS+"mean_68.npy", mean)
+
+
+
+if __name__ == '__main__':
+ lm_pca()
