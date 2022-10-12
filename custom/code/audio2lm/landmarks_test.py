6d86dff15bd9fba151ea695ea0d6a52219e25744
FarZ <fala0009@student.monash.edu>
2022-10-12 18:00:40 +0800

    made changes

---
+++ custom/code/audio2lm/landmarks_test.py
@@ -0,0 +1,66 @@
+import unittest
+import facial_landmark as fl
+from Exception_classes import *
+import dlib
+from unittest.mock import patch
+import extracting_facial_landmarks as f2
+import numpy as np
+from constants import * 
+import os
+
+class FacialLandmarks(unittest.TestCase) :
+
+    def setUp(self) -> None:
+        pass
+
+    def test_extracting_frames(self):
+        video_path="/readme.mp4"
+        with self.assertRaises(PathNotFoundError,msg="video path defined is incorrect"):
+            fl.extracting_frames(video_path,25)
+        video_path = f'test_sample/incorrect_format/first/hello.wav'
+        with self.assertRaises(FileNotFoundError, msg="Incorrect file format"):
+            fl.extracting_frames(video_path, 25)
+        video_path= f'test_sample/correct_video/angry/001.mp4'
+        with self.assertRaises(InvalidNumberofFrames,msg="video does not have given the right number of frames"):
+            fl.extracting_frames(video_path,100)
+        video_path= f'test_sample/correct_video/angry/001.mp4'
+        assert len(fl.extracting_frames(video_path,25))==25
+
+
+    def test_generate_landmarks_frame(self):
+        face_detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor('fit3162_fit3164/shape_predictor_68_face_landmarks.dat')
+        video_path = f'test_sample/correct_video/angry/001.mp4'
+        assert fl.generate_landmarks_frame(fl.extracting_frames(video_path,1)[0]     , face_detector,predictor).shape==(136,)
+
+    def test_facial_landmark(self):
+      video_path =  f'test_sample/correct_video/angry/001.mp4'
+      with patch("facial_landmark.generate_landmarks_frame") as generate:
+            generate.return_value=np.array(136 *[0])
+            landmarks=fl.facial_landmark(video_path,25)
+            assert 25==  generate.call_count
+      assert landmarks.shape==(25,136)
+
+
+    def test_extracting_facial_landmarks(self):
+        # incorrect directory path
+        video_path=f'testing_sample/video/'
+        with self.assertRaises(PathNotFoundError,msg="directory path is incorrect"):
+            f2.extracting_facial_landmarks(video_path)
+
+        # incorrect path name for folder (furious)
+        video_path= f'test_sample/furious'
+        with self.assertRaises(PathNotFoundError, msg="Incorrect vieo path"):
+            f2.extracting_facial_landmarks(video_path)
+
+        # check if file are created if correct path provided
+        video_path=f'test_sample/correct_video'
+        f2.extracting_facial_landmarks(video_path)
+        assert os.path.exists(f'{LM_ENCODER_DATASET_LANDMARK_DIR}/{ACTOR}_angry_3_001/0.npy')==True
+        assert os.path.exists(f'{LM_ENCODER_DATASET_LANDMARK_DIR}/{ACTOR}_angry_3_002/0.npy')==True
+            
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(FacialLandmarks)
+    # This will run the test suite.
+    unittest.TextTestRunner(verbosity=2).run(suite)
