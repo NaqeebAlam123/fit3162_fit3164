1b070d40dbd86f5015543cf1ae42f6a97c08745e
FarZ <fala0009@student.monash.edu>
2022-10-13 03:16:30 +0800

    made changes to help in testing

--- custom/code/audio2lm/extracting_facial_landmarks.py
+++ custom/code/audio2lm/extracting_facial_landmarks.py
@@ -6,7 +6,7 @@ from constants import *
 from config import *
 from Exception_classes import * 
 
-def extracting_facial_landmarks(path):
+def extracting_facial_landmarks(path, test_identifier=False):
     p=Path(path)
     # If path to directory does not exist, raise a PathNotFoundError
     if  not os.path.exists(p):
@@ -28,16 +28,20 @@ def extracting_facial_landmarks(path):
             storepath = f'{LM_ENCODER_DATASET_LANDMARK_DIR}{ACTOR}_{path.name}_3_'
         
 
-    
+        
         for i in range(len(list(path.iterdir()))):
-            array=facial_landmark(str(path.joinpath(str(str(i+1).rjust(3, '0'))+'.mp4')),25)
-            os.makedirs(storepath+ str(str(i+1).rjust(3, '0')),exist_ok=True)
-            np.save(storepath + str(str(i+1).rjust(3, '0')) + '/' + '0.npy', array)
+            if test_identifier==True:
+                array=facial_landmark(str(path.joinpath(str(str(i+1).rjust(3, '0'))+'.mp4')),25)
+                os.makedirs(storepath+ str(str(i+1).rjust(3, '0')),exist_ok=True)
+                np.save(storepath + str(str(i+1).rjust(3, '0')) + '/' + '0.npy', array)
+                break
            #print(i+1)
         print('completed for ' + path.name)
-
+        
+        # check only for one sample in testing
+        if test_identifier==True:
+            break
 
 # paths= f'data/video/{ACTOR}'
 # extracting_facial_landmarks(paths)
-
 
