b1574fa5e7f0903456fb4934bd1bc0fb6a361658
FarZ <fala0009@student.monash.edu>
2022-10-12 17:58:38 +0800

    made changes

--- custom/code/audio2lm/extracting_facial_landmarks.py
+++ custom/code/audio2lm/extracting_facial_landmarks.py
@@ -4,20 +4,31 @@ from facial_landmark import facial_landmark
 from pathlib import Path
 from constants import *
 from config import *
-
-ACTOR = config.actor
+from Exception_classes import * 
 
 def extracting_facial_landmarks(path):
     p=Path(path)
+    # If path to directory does not exist, raise a PathNotFoundError
+    if  not os.path.exists(p):
+        raise PathNotFoundError(path,"Incorrect Directory Path")
+    else:
+        # check the path
+        for paths in p.iterdir():
+            # if path to the vdeo doesn't exist, raise a PathNotFoundError
+            if  not os.path.exists(paths):
+                raise PathNotFoundError(path,"Incorrect Directory Path")
+
+    p=Path(path)        
     for path in p.iterdir():
+    
         print('starting facial landmark extraction for ' + path.name)
         if path.name == 'neutral':
             storepath = f'{LM_ENCODER_DATASET_LANDMARK_DIR}{ACTOR}_{path.name}_1_'
         else:
             storepath = f'{LM_ENCODER_DATASET_LANDMARK_DIR}{ACTOR}_{path.name}_3_'
+        
 
-
-     
+    
         for i in range(len(list(path.iterdir()))):
             array=facial_landmark(str(path.joinpath(str(str(i+1).rjust(3, '0'))+'.mp4')),25)
             os.makedirs(storepath+ str(str(i+1).rjust(3, '0')),exist_ok=True)
@@ -26,5 +37,7 @@ def extracting_facial_landmarks(path):
         print('completed for ' + path.name)
 
 
-paths= f'data/video/{ACTOR}'
-extracting_facial_landmarks(paths)
+# paths= f'data/video/{ACTOR}'
+# extracting_facial_landmarks(paths)
+
+
