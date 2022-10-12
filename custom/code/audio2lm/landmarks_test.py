8c5f1b8f89cea9d1e11fee5925eb29fcbf5610b6
FarZ <fala0009@student.monash.edu>
2022-10-12 19:09:54 +0800

    added comments relevant to the purpose of each test case for each method

--- custom/code/audio2lm/landmarks_test.py
+++ custom/code/audio2lm/landmarks_test.py
@@ -14,12 +14,18 @@ class FacialLandmarks(unittest.TestCase) :
         pass
 
     def test_extracting_frames(self):
+
+        # to check the extracting_frames's methods behaviour if a wrong video path is provided
         video_path="/readme.mp4"
         with self.assertRaises(PathNotFoundError,msg="video path defined is incorrect"):
             fl.extracting_frames(video_path,25)
+        
+        # to check the extracting_frames's methods behaviour if the video is in .mp4 format or not
         video_path = f'test_sample/incorrect_format/first/hello.wav'
         with self.assertRaises(FileNotFoundError, msg="Incorrect file format"):
             fl.extracting_frames(video_path, 25)
+
+        # to check the behaviour of extracting_frames's if wrong number of the frames is provided 
         video_path= f'test_sample/correct_video/angry/001.mp4'
         with self.assertRaises(InvalidNumberofFrames,msg="video does not have given the right number of frames"):
             fl.extracting_frames(video_path,100)
@@ -28,12 +34,14 @@ class FacialLandmarks(unittest.TestCase) :
 
 
     def test_generate_landmarks_frame(self):
+        # to check if the landmark shape being generated from generate_landmarks_frame's method matches the requirement
         face_detector = dlib.get_frontal_face_detector()
         predictor = dlib.shape_predictor('fit3162_fit3164/shape_predictor_68_face_landmarks.dat')
         video_path = f'test_sample/correct_video/angry/001.mp4'
         assert fl.generate_landmarks_frame(fl.extracting_frames(video_path,1)[0]     , face_detector,predictor).shape==(136,)
 
     def test_facial_landmark(self):
+      # to check if the shape of series of landmarks generated from facial_landmark's method amtches the requirement (25,136) where 25 represents to the number of frames and 136 refers to the num of landmark points for each of them
       video_path =  f'test_sample/correct_video/angry/001.mp4'
       with patch("facial_landmark.generate_landmarks_frame") as generate:
             generate.return_value=np.array(136 *[0])
@@ -43,17 +51,17 @@ class FacialLandmarks(unittest.TestCase) :
 
 
     def test_extracting_facial_landmarks(self):
-        # incorrect directory path
+        # to check the behaviour if the wrong directory is passed to extracting_facial_landmarks' method
         video_path=f'testing_sample/video/'
         with self.assertRaises(PathNotFoundError,msg="directory path is incorrect"):
             f2.extracting_facial_landmarks(video_path)
 
-        # incorrect path name for folder (furious)
+        # to check the behaviour if the wrong path name is passed to extracting_facial_landmarks' method
         video_path= f'test_sample/furious'
         with self.assertRaises(PathNotFoundError, msg="Incorrect vieo path"):
             f2.extracting_facial_landmarks(video_path)
 
-        # check if file are created if correct path provided
+        #  to check the behaviour if the right path are passed and to ensure the files generated exist in their respective locations
         video_path=f'test_sample/correct_video'
         f2.extracting_facial_landmarks(video_path)
         assert os.path.exists(f'{LM_ENCODER_DATASET_LANDMARK_DIR}/{ACTOR}_angry_3_001/0.npy')==True
