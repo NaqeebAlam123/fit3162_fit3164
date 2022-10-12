5805d5f1190e5616ae8ecda9e68e16be1b7b095e
FarZ <fala0009@student.monash.edu>
2022-10-12 17:58:09 +0800

    made changes

---
+++ custom/code/audio2lm/Exception_classes.py
@@ -0,0 +1,18 @@
+class PathNotFoundError(Exception):
+    def  __init__(self,path,message,error=0):
+        self.path=path
+        self.message=message
+        self.error_code=error
+        super().__init__(self.message)
+
+    def __str__(self):
+        message=self.message+ " \n Path Defined : "+self.path +"\n Error code : "+ str(self.error_code)
+        return message
+
+
+
+
+class InvalidNumberofFrames(Exception):
+    pass
+
+
