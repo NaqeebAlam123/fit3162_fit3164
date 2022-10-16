f6c451af12c378c62e44396236852156088dac2f
FarZ <fala0009@student.monash.edu>
2022-10-16 22:39:19 +0800

    made changes to import statement for constants.py

--- custom/code/audio2lm/constants.py
+++ custom/code/audio2lm/constants.py
@@ -1,7 +1,13 @@
 import torch
 import os
-from config import config
+import sys 
+ 
+#from fit3162_fit3164.custom.code.audio2lm.config import config
 
+from config import config 
+    
+
+    
 
 def create_dir(path):
     '''
@@ -15,30 +21,28 @@ def create_dir(path):
     return path
 
 
-ACTOR = 'M011'
+ACTOR = 'M030'
 
 EMOTION_NET_DATASET_DIR = create_dir(f'fit3162_fit3164/custom/data/mfcc/{ACTOR}/')
-#EMOTION_NET_MODEL_DIR = create_dir(f'fit3162_fit3164/custom/saved_models/EmotionNet/{ACTOR}/')
+EMOTION_NET_MODEL_DIR = create_dir(f'fit3162_fit3164/custom/saved_models/EmotionNet/{ACTOR}/')
 EMOTION_NET_LOG_DIR = create_dir(f'fit3162_fit3164/custom/log/EmotionNet_original/{ACTOR}/')
 
 
 AUTOENCODER_2X_DATASET_DIR = create_dir(f'fit3162_fit3164/custom/data/emotion_length/{ACTOR}/') ## NOTE: generated from mfcc_dtw.py
-#AUTOENCODER_2X_MODEL_DIR = create_dir(f'fit3162_fit3164/custom/saved_models/AutoEncoder2x/{ACTOR}/')
+AUTOENCODER_2X_MODEL_DIR = create_dir(f'fit3162_fit3164/custom/saved_models/AutoEncoder2x/{ACTOR}/')
 # # AUTOENCODER_2X_RESUME_DIR = create_dir(f'/media/thea/Data/New_exp/3_intensity_{ACTOR}/SER_intensity_3/model/81_pretrain.pth')
 AUTOENCODER_2X_IMAGE_DIR = create_dir(f'fit3162_fit3164/custom/image/{ACTOR}/')
 AUTOENCODER_2X_LOG_DIR = create_dir(f'fit3162_fit3164/custom/log/AutoEncoder2x_pretrained_Ct_encoder/{ACTOR}/')
-
-
 LM_ENCODER_DATASET_LANDMARK_DIR = create_dir(f'fit3162_fit3164/custom/data/landmark/landmark_68/{ACTOR}/generated_landmarks/') ## NOTE: generated from facial_landmark.py
 LM_ENCODER_DATASET_MFCC_DIR = create_dir(f'fit3162_fit3164/custom/data/landmark/mfcc/{ACTOR}/') ## NOTE: generated from lm_pca.py
 LM_ENCODER_MODEL_DIR = create_dir(f'fit3162_fit3164/custom/saved_models/ATEmotion/{ACTOR}/')
 
 
-#LM_ENCODER_PRETRAINED_DIR = f'{AUTOENCODER_2X_MODEL_DIR}2_pretrain.pth'
+LM_ENCODER_PRETRAINED_DIR = f'{AUTOENCODER_2X_MODEL_DIR}2_pretrain.pth'
 LANDMARK_BASICS = create_dir(f'fit3162_fit3164/custom/data/landmark/landmark_68/{ACTOR}/basics/')
 
 ATPRETRAINED_DIR = f'fit3162_fit3164/custom/pretrained_models/atnet_lstm_18.pth' ## NOTE: Ct_encoder pretrai
-#SERPRETRAINED_DIR = f'{EMOTION_NET_MODEL_DIR}/SER_2.pkl'
+SERPRETRAINED_DIR = f'{EMOTION_NET_MODEL_DIR}/SER_2.pkl'
 
 AUDIO_DATASET = create_dir(f'fit3162_fit3164/custom/data/audio/{ACTOR}_wav/')
 ALIGNED_AUDIO_DATA = create_dir(f'fit3162_fit3164/custom/data/aligned_audio/{ACTOR}/')
